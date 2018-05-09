867e329eb36b7b677283a076870dd0634e4d26dc
Jó Sales <jo.salesmj@gmail.com>
2018-05-09 02:08:27 -0300

    Master captura endereço dos dispositivos com disponibilidade para conexão

--- Bluetooth-Master/Bluetooth-Master.ino
+++ Bluetooth-Master/Bluetooth-Master.ino
@@ -1,16 +1,20 @@
 #include<SoftwareSerial.h>
-
 SoftwareSerial mySerial(10,11);
 String command = "";
 String str = "";
+String andress;
 int pinEn = 12;
+int btPower = 9;
 int flag = HIGH;
 void setup(){
   Serial.begin(115200);
   mySerial.begin(38400);
-  pinMode(pinEn, OUTPUT);
+  pinMode(pinEn,OUTPUT);
+  pinMode(btPower,OUTPUT);
+  digitalWrite(btPower,HIGH);
+  delay(100);
   digitalWrite(pinEn,HIGH);
-  delay(1000);
+  delay(100);
   mySerial.write("AT+INIT\r\t");
   if(mySerial.available()){
     while(mySerial.available()){
@@ -22,8 +26,8 @@ void loop(){
   if(flag == HIGH){
     str = "";
     Serial.println("Tentando...");
-    //digitalWrite(pinEn,HIGH);
-    delay(1000);
+    digitalWrite(pinEn,HIGH);
+    delay(400);
     mySerial.write("AT+INQ\r\t");
     if(mySerial.available()){
       while(mySerial.available()){
@@ -31,13 +35,44 @@ void loop(){
       }
     }
     Serial.println(str);
-  }
-  else{
-    Serial.println("Tomando um tempo pra descansar...");
-    for(int i = 0;i<10;i++){
-      Serial.print(10-i);
-      delay(1000);
+    if(str.startsWith("+INQ:")){
+      andress = str.substring(5,str.length() - 12);
+      andress.replace(":",",");
+      Serial.print("Andress:");
+      Serial.println(andress);
+      String aux = "";
+      String link = "AT+BIND=" + andress + "\r\t";
+      String link1 = "AT+LINK" + andress + "\r\t";
+      char link2[link.length()];
+      char link3[link.length()];
+      link.toCharArray(link2,link.length());
+      link1.toCharArray(link3,link1.length());
+      mySerial.write("AT+CMODE=1\r\t");
+      delay(100);
+      mySerial.write(link2);
+      delay(100);
+      mySerial.write(link3);
+      if(mySerial.available()){
+      while(mySerial.available()){
+        aux += (char)mySerial.read();
+      }
+      Serial.print("Aux: ");Serial.println(aux);
+      flag = !flag;
+      }
     }
-    flag = !flag;
+  }
+  else if(flag == LOW){
+    digitalWrite(pinEn,LOW);
+    delay(100);
+   // int n = 8 + andress.length();
+  //  char link1[n];// = "AT+LINK=";
+//    link.toCharArray(link1,link.length());
+    //sprintf(link1,andress);
+    //mySerial.write(link1);
+    if(mySerial.available()){
+      while(mySerial.available()){
+        str += (char)mySerial.read();
+      }
+    }
   }
 }
