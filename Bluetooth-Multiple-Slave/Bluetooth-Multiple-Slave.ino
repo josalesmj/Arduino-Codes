f7f87f1459808cb071b487e210f0e158d7275e2c
Jó Sales <32043542+kaminarirn@users.noreply.github.com>
2018-05-24 22:51:16 -0300

    Update Bluetooth-Multiple-Slave.ino

--- Bluetooth-Multiple-Slave/Bluetooth-Multiple-Slave.ino
+++ Bluetooth-Multiple-Slave/Bluetooth-Multiple-Slave.ino
@@ -3,16 +3,18 @@
 boolean flag = HIGH;
 int pinEn = 12;
 int btPower = 9;
-String command = "";
+String str = "";
 SoftwareSerial mySerial(10, 11); //RX, TX
+  String address = "";
+  String acao = "";
+
 
 void setup(){
   Serial.begin(115200);
   pinMode(pinEn, OUTPUT);  
   pinMode(btPower, OUTPUT);
-  
-  digitalWrite(btPower, LOW);
-  digitalWrite(pinEn, LOW);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
   
   /************************************************
   Setting the pins to low is important because 
@@ -22,106 +24,111 @@ void setup(){
   therefore many people have problems with getting 
   into the AT mode of the HC-05
   ************************************************/
-  delay(100);
-  digitalWrite(pinEn, HIGH);
-  delay(100);
-
-  //Liga o HC-05
-  digitalWrite(btPower, HIGH);
-  delay(500);
-  mySerial.begin(38400);
-  
-  // Send an "AT" command to the AT (without quotes)
-  // if response is OK, then we are connected
-  // and ready to program the BT module
-  delay(3000);
-  mySerial.write("AT+ROLE=0\r\n");
+  putInSlaveMode();
   Serial.println("Aguardando por conexão");
-  delay(1000);
-  digitalWrite(pinEn,LOW);
-  digitalWrite(btPower,LOW);
-  delay(500);
-  digitalWrite(btPower,HIGH);
-  delay(500);
  }
 
 void loop(){
+  str = "";
   //Aguardando por ordem
   if(flag == HIGH){
+    digitalWrite(5,LOW);
+    digitalWrite(6,HIGH);
     if(mySerial.available()){
       while(mySerial.available()){
-        command += (char)mySerial.read();
+        str += (char)mySerial.read();
       }
-      Serial.println(command);  
+      Serial.println(str);  
     }
     if(Serial.available()){
       delay(10);
       mySerial.write(Serial.read());
     }
-    command = "";
+    if(str.startsWith("comd:")){
+      address = str.substring(5,str.length()-2);
+      acao = str.substring(str.length()-1);
+      Serial.print("Address: ");Serial.println(address);
+      Serial.print("Açao: ");Serial.println(acao);
+      flag = !flag;
+    }
   }
-
   else{
-    mySerial.write("AT+BIND=98D3,31,F50EEC\r\n");
-    Serial.write("AT+BIND=98D3,31,F50EEC\r\n");
-    delay(2000);
-    mySerial.write("AT+INIT\r\n");
-    Serial.write("AT+INIT\r\n");
-    delay(3000);
-    mySerial.write("AT+INQ\r\n");
-    Serial.write("AT+INQ\r\n");
-    delay(4000);
-    mySerial.write("AT+LINK=98D3,31,F50EEC\r\n");   
-    Serial.write("AT+LINK=98D3,31,F50EEC\r\n");   
-    delay(4000);    
-    
-    mySerial.write("on");
-    delay(2000);
-    mySerial.write("off");
-    delay(4000);
-    digitalWrite(btPower, LOW);
-    digitalWrite(pinEn, LOW);
-    delay(100);
-    digitalWrite(pinEn, HIGH);
-    delay(100);
-    digitalWrite(btPower, HIGH);
-    Serial.write("Desconectando\r\n");
-    delay(4000);
-  
-  
-    mySerial.write("AT+BIND=12,9,139146\r\n");
-    Serial.write("AT+BIND=12,9,139146\r\n");
-    delay(2000);
-    mySerial.write("AT+INIT\r\n");
-    Serial.write("AT+INIT\r\n");
-    delay(3000);
-    mySerial.write("AT+INQ\r\n");
-    Serial.write("AT+INQ\r\n");
-    delay(4000);
-    mySerial.write("AT+LINK=12,9,139146\r\n");   
-    Serial.write("AT+LINK=12,9,139146\r\n");   
-    delay(4000);    
-  
-    mySerial.write("on");
-    delay(2000);
-    mySerial.write("off");
-    delay(4000);
-    digitalWrite(btPower, LOW);
-    digitalWrite(pinEn, LOW);
-    delay(100);
-    digitalWrite(pinEn, HIGH);
-    delay(100);
-    digitalWrite(btPower, HIGH);
-    Serial.write("Desconectando\r\n");
-    delay(4000);
-  
-
-    // listen for a response from the HC-05 and write it to the serial monitor
-    if (mySerial.available())
-      Serial.write(mySerial.read());
-
-    // listen for user input and send it to the HC-05
-    if (Serial.available())
-      mySerial.write(Serial.read());
+    digitalWrite(5,HIGH);
+    digitalWrite(6,LOW);
+    comandaDevice(address,acao);
+    delay(500);
   }
+}
+
+void comandaDevice(String address,String acao){
+  putInMasterMode();
+  mySerial.print("AT+CMODE=1\r\n");
+  delay(500);
+  String aux = "AT+BIND=" + address + "\r\n";
+  mySerial.print(aux);
+  Serial.print(aux);
+  waitForResponse();
+  mySerial.print("AT+INIT\r\n");
+  Serial.println("AT+INIT\r\n");
+  waitForResponse();
+  mySerial.print("AT+INQ\r\n");
+  Serial.println("AT+INQ\r\n");
+  waitForResponse();
+  aux = "AT+LINK=" + address + "\r\n";
+  mySerial.print(aux);
+  Serial.println(aux);
+  waitForResponse();
+  acao += "\r\n";
+  mySerial.print(acao);
+  waitForResponse();
+  digitalWrite(btPower, LOW);
+  digitalWrite(pinEn, LOW);
+  delay(100);
+  digitalWrite(pinEn, HIGH);
+  delay(100);
+  digitalWrite(btPower, HIGH);
+  Serial.println("Desconectando");
+  delay(1000);
+  putInSlaveMode();
+  flag = !flag;
+}
+void putInATCommands(){
+  digitalWrite(btPower,LOW);
+  digitalWrite(pinEn,LOW);
+  delay(100);
+  digitalWrite(pinEn,HIGH);
+  delay(100);
+  digitalWrite(btPower,HIGH);
+  delay(500);
+  mySerial.begin(38400);
+  delay(3000);
+}
+
+void putInMasterMode(){
+  putInATCommands();
+  mySerial.write("AT+ROLE=1\r\n");
+  delay(500);
+  mySerial.write("AT+CMODE=0\r\n");
+  digitalWrite(pinEn,LOW);
+  digitalWrite(btPower,LOW);
+  delay(500);
+  digitalWrite(btPower,HIGH);
+  delay(500);
+}
+void putInSlaveMode(){
+  putInATCommands();
+  mySerial.write("AT+ROLE=0\r\n");
+  delay(500);
+  digitalWrite(pinEn,LOW);
+  digitalWrite(btPower,LOW);
+  delay(500);
+  digitalWrite(btPower,HIGH);
+  delay(500);
+}
+void waitForResponse(){
+  delay(1000);
+  while (mySerial.available()) {
+    Serial.write(mySerial.read());
+  }
+  Serial.write("\n");
 }
